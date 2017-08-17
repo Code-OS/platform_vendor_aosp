39f493ea325eaccb0c1abffa6adb5271d3e7adb1
shivatejapeddi <shivatejapeddi@gmail.com>
2017-08-17 15:25:00 +0000

    Update cos

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,6 +1,6 @@
 # Inherit common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/cos/config/common.mk)
 
-$(call inherit-product, vendor/aosp/config/telephony.mk)
+$(call inherit-product, vendor/cos/config/telephony.mk)
 
-$(call inherit-product, vendor/aosp/config/cos_props.mk)
+$(call inherit-product, vendor/cos/config/cos_props.mk)
